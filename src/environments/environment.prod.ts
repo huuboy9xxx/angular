01b9c77aa5016541aaa6a1b30ca21977ad3fcf13
Hoang Van Huy <huynoone@gmail.com>
2020-04-14 09:20:06 +0700

    cm

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   firebase : {
-    apiKey: "AIzaSyAU2II20VaOV2KEMznTJmSKLKl6jaS3Gkw",
-    authDomain: "angular-302f9.firebaseapp.com",
-    databaseURL: "https://angular-302f9.firebaseio.com",
-    projectId: "angular-302f9",
-    storageBucket: "angular-302f9.appspot.com",
-    messagingSenderId: "1064087464787",
-    appId: "1:1064087464787:web:6a6dd1e4909b04a2390fa6",
-    measurementId: "G-521K6R5CEQ"
+    // apiKey: "AIzaSyAU2II20VaOV2KEMznTJmSKLKl6jaS3Gkw",
+    // authDomain: "angular-302f9.firebaseapp.com",
+    // databaseURL: "https://angular-302f9.firebaseio.com",
+    // projectId: "angular-302f9",
+    // storageBucket: "angular-302f9.appspot.com",
+    // messagingSenderId: "1064087464787",
+    // appId: "1:1064087464787:web:6a6dd1e4909b04a2390fa6",
+    // measurementId: "G-521K6R5CEQ"
   }
 };
